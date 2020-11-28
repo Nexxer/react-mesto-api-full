809ca87193ce1b1b65a7649e957f9a9c6d7378c4
Nexxer <yairopb@yandex.ru>
2020-11-28 16:25:37 +0300

    new message err

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500
-      ? 'serverError'
+      ? 'На сервере произошла ошибка!'
       : message,
   });
   next();
